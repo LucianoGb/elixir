3264eaf0919037422cbe5c52852fc0be2d2795e2
Luciano Gouveia <ljdgb2012@gmail.com>
2022-03-23 15:11:38 -0300

    Atualiza.

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,3 +1,18 @@
 defmodule ExMon do
- 
+  # Quando usamos o alias, o ultimo nome será o Alias.
+  alias ExMon.{Player, Games}
+
+  # alias renomado
+  # alias ExMon.Player, as: Novo
+
+  def create_player(name, move_avg, move_rnd, move_heal) do
+    Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    "Robotinik"
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+  end
+
 end
